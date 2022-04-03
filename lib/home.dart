34b9fdcc73fd278ee9f28ec127e6a893fe390447
Shtajnbah <eshtacznbax@bk.ru>
2022-04-03 19:10:09 +0300

    Card3

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'card1.dart';
 import 'card2.dart';
+import 'card3.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -11,17 +12,12 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   int _selectedIndex = 0;
-
   // _ - приватная переменная
   // отражает какая вкладка выбрана в данный момент
-
-// 8
   static List<Widget> pages = <Widget>[
     const Card1(),
     const Card2(),
-    Container(color: Colors.green),
-    // TODO: Replace with Card3
-    Container(color: Colors.blue)
+    const Card3()
   ];
 
 // 9
